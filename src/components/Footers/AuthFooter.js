3c364d3c7d14663adfc1c03a54de383479fd4400
danielccosta9 <danielc.costa9@gmail.com>
2022-05-30 16:18:27 -0300

    Interface

---
+++ src/components/Footers/AuthFooter.js
@@ -0,0 +1,28 @@
+import { Container, Row, Col } from "reactstrap";
+
+const Login = () => {
+  return (
+    <>
+      <footer className="py-5">
+        <Container>
+          <Row className="align-items-center justify-content-xl-between">
+            <Col xl="6">
+              <div className="copyright text-center text-xl-left text-muted">
+                © {new Date().getFullYear()}{" "}
+                <a
+                  className="font-weight-bold ml-1"
+                  href="https://www.creative-tim.com?ref=adr-auth-footer"
+                  target="_blank"
+                >
+                  IFPB
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
+
+import AdminLayout from "layouts/Admin.js";
+import AuthLayout from "layouts/Auth.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
